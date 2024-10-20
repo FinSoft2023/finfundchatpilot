6fb6d9fd5c3137d804c9903c2effa60e51e1363c
Teerachai Laothong <teerachai@finsoft2023.com>
2024-10-20 11:56:41 +0700

    Add about and chat links to index page

--- src/apps/facingweb/pages/index.vue
+++ src/apps/facingweb/pages/index.vue
@@ -2,5 +2,8 @@
   <div>
     <h1>Welcome to Chat start project</h1>
     <h2>This project is inside the (facingweb)</h2>
+    <p>Click on the link below to go to the chat page</p>
+    <NuxtLink to="/chat">Go to chat</NuxtLink>
+    <NuxtLink to="/about">Go to about</NuxtLink>
   </div>
 </template>

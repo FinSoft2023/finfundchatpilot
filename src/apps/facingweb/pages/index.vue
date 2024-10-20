9631a2ed45e80dfce364aeaa41704f93139e75a6
Teerachai Laothong <teerachai@finsoft2023.com>
2024-10-20 15:19:33 +0700

    Refactor welcome message and add project purpose

--- src/apps/facingweb/pages/index.vue
+++ src/apps/facingweb/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h1>Welcome to Chat start project</h1>
+    <h1>Welcome to Chat starter project</h1>
     <h2>This project is inside the (facingweb)</h2>
+    <p>The purpose of this project is for piloting the chat app</p>
     <p>Click on the link below to go to the chat page</p>
     <ul>
       <li>

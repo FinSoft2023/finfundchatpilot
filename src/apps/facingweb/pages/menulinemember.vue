38e732200d830eef121d89cd618fa031491cc868
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-10-23 14:15:33 +0700

    concept prototype menu line #11

---
+++ src/apps/facingweb/pages/menulinemember.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="p-4">
+         <h1 @click="links" class="text-xl mb-4 font-bold">Line OA</h1>
+        <div class="grid grid-cols-2 gap-4">
+            <UPageCard v-for="(module, index) in modules"
+                :key="index"
+                class="text-red-500"
+                v-bind="module">
+            </UPageCard>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const links = () => {
+    navigateTo('/menuline');
+}
+
+const modules = [
+    {
+        title: 'สแกน',
+        to: '/transaction',
+        icon: 'i-mdi-scan-helper',
+    },
+    {
+        title: 'ยืนยันตัวตน',
+        to: '',
+        icon: 'i-mdi-account-lock-outline',
+    },
+    {
+        title: 'ข้อมูลสมาชิก',
+        to: '',
+        icon: 'i-mdi-user-card-details-outline',
+    },
+    {
+        title: 'เงินฝาก',
+        to: '',
+        icon: 'i-heroicons-wallet',
+    },
+    {
+        title: 'กู้ฉุกเฉิน',
+        to: '',
+        icon: 'i-mdi-cash-fast',
+    },
+    {
+        title: 'สัญญากู้',
+        to: '',
+        icon: 'i-mdi-file-document',
+    },
+    {
+        title: 'ฌาปนกิจ',
+        to: '',
+        icon: 'i-mdi-account-child',
+    },
+    {
+        title: 'รายการหักเดือนล่าสุด',
+        to: '',
+        icon: 'i-mdi-report-box-outline',
+    },
+    {
+        title: 'เงินปันผลและเฉลี่ยคืน',
+        to: '',
+        icon: 'i-mdi-dollar',
+    },
+    {
+        title: 'ตั้งค่าความปลอดภัย',
+        to: '',
+        icon: 'i-mdi-password',
+    },
+];
+</script>
---
+++ src/apps/facingweb/pages/prepare.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="p-4">
+         <h1 @click="links" class="text-xl font-bold">Line OA</h1>
+         <p class="text-base mb-4 font-semibold">เตรียมข้อมูล</p>
+        <UCard v-for="(module, index) in modules">
+            <UIcon :name="module.icon"
+                class="w-10 h-10 text-primary" />
+            <h1 class="text-lg font-bold">{{ module.title }}</h1>
+            <div class="flex space-x-2 items-center">
+                <UIcon :name="module.check"
+                    class="w-5 h-5"></UIcon>
+                <p>ยังไม่ได้เตรียมข้อมูล</p>
+            </div>
+
+            <UButton color="red"
+                :to="module.to"
+                block>กรอกใบสมัคร</UButton>
+        </UCard>
+    </div>
+</template>
+
+<script setup lang="ts">
+const links = () => {
+    navigateTo('/menuline');
+}
+const modules = [
+    {
+        title: 'สมัครสมาชิก',
+        to: '/subscription',
+        icon: 'i-mdi-user-add',
+        check: 'i-mdi-check-circle',
+    },
+];
+</script>
